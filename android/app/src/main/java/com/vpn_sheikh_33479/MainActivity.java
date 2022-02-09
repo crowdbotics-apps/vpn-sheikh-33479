60e315e25505c22d7d256ecac3c44d9dd269947a
Crowdbotics <team@crowdbotics.com>
2022-02-09 20:01:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vpn_sheikh_33479/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vpn_sheikh_33479;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vpn_sheikh_33479";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vpn_sheikh_33479",
+  url: "https://vpn_sheikh_33479.botics.co",
+  api: "https://vpn_sheikh_33479.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
